fd0d77d6583e7bee691a996bba3128c2d9ec47c6
jamoliddinbe <bosuvay1@gmail.com>
2022-09-20 17:28:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+// Ixtiyor domlani misollari
+
+
+
+
+function optinal(attendet, lenght){
+    if (typeof attendet, lenght === "number" || typeof attendet, lenght === "string" && attendet, lenght > 0 && attendet >= lenght ) {
+        console.log(Number(attendet));
+        console.log(Number(lenght));
+
+// tipini ozgartiryapganini isbotlash uchun
+
+        // console.log(attendet);
+        // console.log(lenght);
+
+// tipini ozgartiryapganini isbotlash uchun
+      
+        return true
+    } else{
+        return false
+    }
+
+}
+
+console.log(optinal(12, '11')); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function yigindi(n){
+// let sum = 0;
+// for(let i = 0; i <= n; i++  ){
+//     sum +=  i
+// }
+// return sum
+// }
+// console.log(yigindi(10));
+
